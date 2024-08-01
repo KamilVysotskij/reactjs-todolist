ebb586a592c76613a4a740aa9ea91ba57096d3d2
Kamil Vysotskij <kamil.vysotskij@yandex.ru>
2024-08-01 15:56:39 +0500

    first react attempt

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react"
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
+
+function App() {
+// return jsx, you can write JS straight inside the jsx
+  const [todos, setTodos] = useState([])
+  const [todoValue, setTodoValue] = useState('')
+
+  function persistData(newList) {
+    localStorage.setItem('toods', JSON.stringify({ todos:newList }))
+  }
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
+
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index
+    })
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
+
+  function hadleEditTodo(index) {
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodo(index)
+  }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return
+    }
+
+    let localTodos = localStorage.getItem('toods')
+    if (!localTodos) {
+      return
+    }
+
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
+  }, [])
+
+  return (
+    <>
+      <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos}/>
+      <TodoList hadleEditTodo={hadleEditTodo} handleDeleteTodo={handleDeleteTodo} todos={todos} />
+    </>
+  )
+}
+
+export default App
+// App is the highest level parent component
